cbc96f1c228b526023a30ff7fcea9f9d2bedec72
almafwilgom <gomenoch@gmail.com>
2026-05-05 05:11:11 +0100

    Add Cloudflare domain to CORS whitelist

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'http://localhost:5000',
+  'https://abs-atv.pages.dev',
 ];
 
 // Add Render frontend if specified
